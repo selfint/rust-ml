9e3e7e55971c2e39e3d551dff5e172e286e83c25
Tom Selfin <selfint@gmail.com>
2021-01-18 22:18:34 +0200

    move neuro evolution Evolve trait into submodule on re-export

--- src/rl/learners/mod.rs
+++ src/rl/learners/mod.rs
@@ -2,4 +2,7 @@ mod learner;
 mod neuro_evolution;
 
 pub use learner::Learner;
-pub use neuro_evolution::{Evolve, NeuroEvolutionLearner};
+pub use neuro_evolution::NeuroEvolutionLearner;
+pub mod neuro_evolution_internals {
+    pub use super::neuro_evolution::Evolve;
+}
