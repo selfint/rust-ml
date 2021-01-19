3feb466d9b0028bde65809c1d45afe925a9cb8ff
Tom Selfin <selfint@gmail.com>
2021-01-19 22:11:51 +0200

    cargo fmt

--- src/neuron/losses/mean_squared_error.rs
+++ src/neuron/losses/mean_squared_error.rs
@@ -5,20 +5,12 @@ use crate::neuron::losses::{Loss, LossTrait};
 #[derive(Clone, Debug)]
 pub struct MSE;
 
-pub fn mse(
-        prediction: &Array1<f32>,
-        expected: &Array1<f32>,
-    ) -> f32 {
+pub fn mse(prediction: &Array1<f32>, expected: &Array1<f32>) -> f32 {
     (prediction - expected).map(|e| e * e).mean().unwrap()
 }
 
-
 impl LossTrait for MSE {
-    fn loss(
-        &self,
-        prediction: &Array1<f32>,
-        expected: &Array1<f32>,
-    ) -> f32 {
+    fn loss(&self, prediction: &Array1<f32>, expected: &Array1<f32>) -> f32 {
         mse(prediction, expected)
     }
 }
