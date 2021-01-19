633eb87f508ce517cc21a06055b5cbf1d7ce9bc0
Tom Selfin <selfint@gmail.com>
2021-01-19 10:57:53 +0200

    move global types to prelude from mod.rs
    Also refactor Action into a trait instead of an enum.

--- src/rl/mod.rs
+++ src/rl/mod.rs
@@ -2,17 +2,3 @@ pub mod agents;
 pub mod environments;
 pub mod learners;
 pub mod prelude;
-
-use ndarray::Array1;
-
-/// Actions that agents can take in an environment
-pub enum Action {
-    Discrete(usize),
-    Continuous(Array1<f32>),
-}
-
-/// How an environment is observed
-pub type State = Array1<f32>;
-
-/// How an environment rewards an agent for an action
-pub type Reward = f32;
--- src/rl/prelude.rs
+++ src/rl/prelude.rs
@@ -1,4 +1,24 @@
-pub use crate::rl::agents::agent::Agent;
+use std::fmt::Debug;
+
+pub use crate::rl::agents::Agent;
 pub use crate::rl::environments::Environment;
 pub use crate::rl::learners::Learner;
-pub use crate::rl::{Action, Reward, State};
+
+use ndarray::Array1;
+
+/// Actions that agents can take in an environment
+pub trait Action: Debug + Clone {}
+
+#[derive(Debug, Clone, Copy)]
+pub struct DiscreteAction(pub usize);
+impl Action for DiscreteAction {}
+
+#[derive(Debug, Clone)]
+pub struct ContinuousAction(pub Array1<f32>);
+impl Action for ContinuousAction {}
+
+/// How an environment is observed
+pub type State = Array1<f32>;
+
+/// How an environment rewards an agent for an action
+pub type Reward = f32;
