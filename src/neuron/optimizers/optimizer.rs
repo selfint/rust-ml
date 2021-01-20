6ac2e274784c7e012adad06b9bd3741f710744ff
Tom Selfin <selfint@gmail.com>
2021-01-20 14:14:47 +0200

    update optimizer trait
    maybe in the future have separate optimizations
    for cached and uncached networks?

--- src/neuron/optimizers/optimizer.rs
+++ src/neuron/optimizers/optimizer.rs
@@ -1,12 +1,12 @@
 use ndarray::Array1;
 
 use crate::neuron::layers::Cached;
-use crate::neuron::networks::FeedForwardNetworkTrait;
+use crate::neuron::networks::CachedNetworkTrait;
 
 pub trait OptimizeOnce<N, L>: Clone
 where
     L: Cached,
-    N: FeedForwardNetworkTrait<L>,
+    N: CachedNetworkTrait<L>,
 {
     fn optimize_once(&self, network: &mut N, prediction: &Array1<f32>, expected: &Array1<f32>);
 }
