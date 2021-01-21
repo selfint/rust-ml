0ce8692859ebea5df65c157ad802833d80ad267e
Tom Selfin <selfint@gmail.com>
2021-01-21 13:25:57 +0200

    add activation changes to mod rs
    Add new activation functions to mod rs
    Export derivative functions as well as activations

--- src/neuron/activations/mod.rs
+++ src/neuron/activations/mod.rs
@@ -1,11 +1,15 @@
 mod activation;
+mod leaky_relu;
 mod linear;
 mod relu;
 mod sigmoid;
 mod softmax;
+mod softplus;
 
 pub use activation::{Activation, ActivationTrait};
-pub use linear::{linear_activation, Linear};
-pub use relu::{relu_activation, ReLu};
-pub use sigmoid::{sigmoid_activation, Sigmoid};
-pub use softmax::{softmax_activation, Softmax};
+pub use leaky_relu::{leaky_relu_activation, leaky_relu_derivate, LeakyReLu};
+pub use linear::{linear_activation, linear_derivative, Linear};
+pub use relu::{relu_activation, relu_derivative, ReLu};
+pub use sigmoid::{sigmoid_activation, sigmoid_derivative, Sigmoid};
+pub use softmax::{softmax_activation, softmax_derivative, Softmax};
+pub use softplus::{softplus_activation, softplus_derivative, Softplus};
