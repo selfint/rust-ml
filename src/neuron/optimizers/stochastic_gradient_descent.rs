1c6470d6ef6fe09792420dfb4eea464e0a2170da
Tom Selfin <selfint@gmail.com>
2021-01-21 13:32:06 +0200

    fix sgd implementation and add documentation

--- src/neuron/optimizers/stochastic_gradient_descent.rs
+++ src/neuron/optimizers/stochastic_gradient_descent.rs
@@ -30,45 +30,70 @@ impl SGD {
         N: CachedNetworkTrait<L>,
     {
         let layers = network.get_layers();
-        let mut dc_da = self.loss.derivative(prediction, expected);
 
         let mut network_weights_gradients = vec![];
         let mut network_biases_gradients = vec![];
-        for i in (0..network.len()).rev() {
-            let layer = &layers[i];
-            let layer_weights = layer.get_weights();
-            let layer_transfers = layer.get_transfer().unwrap();
-            let activation_gradient = layer.apply_activation_derivative(layer_transfers);
 
-            let previous_layer_activations = if i == 0 {
-                layer.get_input().unwrap()
-            } else {
-                layers[i - 1].get_activation().unwrap()
-            };
+        // derivative of the loss with respect to the last layers activation
+        let mut dl_da = Box::new(self.loss.derivative(prediction, expected));
 
-            // weights and biases gradients
-            let layer_outputs = layer.output_size();
-            let layer_inputs = layer.input_size();
+        for layer in layers.iter().rev() {
+            // derivative of the activations with respect to the transfers
+            let da_dt = layer.apply_activation_derivative(layer.get_transfer().unwrap());
 
-            let da_dt = &activation_gradient;
-            let layer_biases_gradient = dc_da * da_dt;
+            // calculate bias gradients
+            // chain rule - derivative of the loss with respect to the biases
+            //
+            // derivative of the transfers with respect to the biases (dt_db) is 1
+            // so da_db = da_dt * dt_db = da_dt
+            let da_db = &da_dt;
 
-            let prev_dc_da = (dc_da * da_dt.dot(layer_weights)).sum();
-
-            let mut layer_weights_gradients = Array2::zeros((layer_outputs, layer_inputs));
-            for j in 0..layer_outputs {
-                for k in 0..layer_inputs {
-                    let dt_dw = previous_layer_activations[k];
-                    layer_weights_gradients[[j, k]] += (dc_da * da_dt * dt_dw).sum();
-                }
-            }
-
-            network_weights_gradients.insert(0, layer_weights_gradients);
+            let layer_biases_gradient: Array1<f32> = dl_da.as_ref() * da_db;
             network_biases_gradients.insert(0, layer_biases_gradient);
 
-            // BACK PROPAGATION: update dc_da to derivative of the cost with respect
-            // to the activation of the previous layer
-            dc_da = prev_dc_da;
+            // calculate weight gradients
+            // TODO: convert this to matrix multiplication - issue #35
+            let layer_outputs = layer.output_size();
+            let layer_inputs = layer.input_size();
+            let mut layer_weights_gradients = Array2::zeros((layer_outputs, layer_inputs));
+
+            // derivative of the transfer with respect to the weights•
+            let dt_dw = layer.get_input().unwrap();
+            for j in 0..layer_outputs {
+                for k in 0..layer_inputs {
+                    // derivate of transfer with respect to this weight
+                    let dt_dw_jk = dt_dw[k];
+
+                    // derivative of the activation of the weight's dst node
+                    // with respect to the transfer of the weight's src node
+                    let da_dt_j = da_dt[j];
+
+                    // derivative of the loss with respect to the activation of
+                    // this weight's dst noe
+                    let dl_da_j = dl_da[j];
+
+                    // chain rule - derivative of the loss with respect to this weight
+                    layer_weights_gradients[[j, k]] = dl_da_j * da_dt_j * dt_dw_jk;
+                }
+            }
+            network_weights_gradients.insert(0, layer_weights_gradients);
+
+            // calculate previous layer loss
+            // derivative of the transfers with respect to the previous layer's
+            // activations - these are all the weights from each node in the
+            // previous layer
+            let dt_dap: &Array2<f32> = &layer.get_weights();
+
+            // chain rule - derivate of loss with respect to previous layer's
+            // activations
+            //
+            // since each node in the previous can affect multiple nodes in this layer
+            // the derivative is summed over all nodes affected in this layer (Axis(0))
+            let dl_da_prev: Array1<f32> = dl_da.sum() * &da_dt.sum() * dt_dap.sum_axis(Axis(0));
+
+            // BACK PROPAGATION: set dc_da as the previous layer's dc_da, propagating
+            // the loss back to the previous layers
+            dl_da = Box::new(dl_da_prev);
         }
 
         (network_weights_gradients, network_biases_gradients)
@@ -96,7 +121,7 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::neuron::activations::Sigmoid;
+    use crate::neuron::activations::{ReLu, Sigmoid, Softplus, LeakyReLu};
     use crate::neuron::layers::CachedLayer;
     use crate::neuron::losses::{mse_loss, MSE};
     use crate::neuron::networks::{CachedNetwork, CachedNetworkTrait, FeedForwardNetworkTrait};
@@ -105,29 +130,33 @@ mod tests {
     #[test]
     fn test_sgd_optimize_once_convergence() {
         let mut network = CachedNetwork::new(vec![
-            CachedLayer::new(3, 2, FullyConnected::new(), Sigmoid::new()),
-            CachedLayer::new(2, 3, FullyConnected::new(), Sigmoid::new()),
+            CachedLayer::new(3, 2, FullyConnected::new(), Softplus::new()),
+            CachedLayer::new(4, 3, FullyConnected::new(), ReLu::new()),
+            CachedLayer::new(5, 4, FullyConnected::new(), Sigmoid::new()),
+            CachedLayer::new(6, 5, FullyConnected::new(), LeakyReLu::new()),
         ]);
 
         let input = array![1., 0.];
-        let expected = array![0.4, 0.6];
+        let expected = array![0.0, 0.2, 0.4, 0.6, 0.8, 1.0];
 
-        let optimizer = SGD::new(1., MSE::new());
+        let optimizer = SGD::new(0.1, MSE::new());
 
-        let mut prediction = network.predict(&input);
-        for _ in 0..100 {
-            prediction = network.predict_cached(&input);
+        for _ in 0..200 {
+            let prediction = network.predict_cached(&input);
             optimizer.optimize_once(&mut network, &prediction, &expected);
         }
 
+        let prediction = network.predict(&input);
+        let cost = mse_loss(&prediction, &expected).sum();
         eprintln!(
             "prediction: {} expected: {}",
             prediction.to_string(),
             expected.to_string()
         );
         assert!(
-            mse_loss(&prediction, &expected) <= 0.1,
-            "optimizer failed to converge"
+            cost <= 0.0001,
+            "optimizer failed to converge (cost: {}>0.0001)",
+            cost
         );
     }
 }
