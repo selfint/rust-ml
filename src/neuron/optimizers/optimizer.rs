abfb1db4368023733f71be066a612f8064c263ef
Tom Selfin <selfint@gmail.com>
2021-01-21 15:59:14 +0200

    add optimize batch trait

--- src/neuron/optimizers/optimizer.rs
+++ src/neuron/optimizers/optimizer.rs
@@ -10,3 +10,11 @@ where
 {
     fn optimize_once(&self, network: &mut N, input: &Array1<f32>, expected: &Array1<f32>);
 }
+
+pub trait OptimizeBatch<N, L>: Clone
+where
+    L: Cached,
+    N: CachedNetworkTrait<L>,
+{
+    fn optimize_batch(&self, network: &mut N, batch_inputs: &[Array1<f32>], batch_expected: &[Array1<f32>]);
+}
