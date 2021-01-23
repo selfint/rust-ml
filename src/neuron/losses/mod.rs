866e4b11d1bbadc9e55217e788775fdc246a1671
tomaster777 <selfint@gmail.com>
2021-01-23 20:18:50 +0200

    add sum squared error loss

--- src/neuron/losses/mod.rs
+++ src/neuron/losses/mod.rs
@@ -1,5 +1,7 @@
 mod loss;
 mod mean_squared_error;
+mod sum_squared_error;
 
 pub use loss::{Loss, LossTrait};
 pub use mean_squared_error::{mse_loss, MSE};
+pub use sum_squared_error::{sse_loss, SSE};
