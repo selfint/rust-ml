8650c1bed81e1a88b6de343d11c2df34f9b72b60
tomaster777 <selfint@gmail.com>
2021-02-15 21:43:25 +0200

    implement q table flappy example - closes #43

--- examples/q_table_flappy.rs
+++ examples/q_table_flappy.rs
@@ -2,7 +2,9 @@ use rust_ml::rl::prelude::*;
 
 use std::{thread, time};
 
-use rust_ml::neuron::activations::ReLu;
+use ndarray::prelude::*;
+
+use rust_ml::neuron::activations::{LeakyReLu, Sigmoid, Softplus};
 use rust_ml::neuron::layers::Dense;
 use rust_ml::neuron::networks::StandardFeedForwardNetwork;
 
@@ -14,53 +16,37 @@ use rust_ml::rl::trainers::q_learning::{QFunction, QLearner};
 
 fn main() {
     // build training environment
-    let env_size = 10;
-    let env = FlappyEnvironment::new(env_size);
+    let env_size = 12;
+    let hole_size = 3;
+    let env = FlappyEnvironment::new(env_size, hole_size);
 
-    // Q learning
-    let qagent = QTableAgent::new(env.action_space());
+    let mut agent = QTableAgent::new(env.action_space());
 
-    let epsilon_decay_rate = 0.99;
+    let epsilon_decay_rate = 0.999;
     let gamma = 0.9;
-    let learning_rate = 0.8;
-    let qtrainer = QLearner::new(epsilon_decay_rate, learning_rate, gamma);
+    let learning_rate = 0.3;
+    let mut trainer = QLearner::new(epsilon_decay_rate, learning_rate, gamma);
 
-    let q = (qagent, qtrainer);
-
-    // Neuro evolution
-    let network = StandardFeedForwardNetwork::new(vec![
-        Dense::new(5, env.observation_space(), ReLu::new()),
-        Dense::new(env.action_space(), 5, ReLu::new()),
-    ]);
-    let nagent = NeuroEvolutionAgent::new(network);
-
-    let agent_amount = 50;
-    let mutation_rate = 0.3;
-    let ntrainer = GeneticAlgorithm::new(agent_amount, mutation_rate);
-
-    let n = (nagent, ntrainer);
-
-    // train agents
-    let (mut agent, mut trainer) = n;
-
-    let epochs = 1000;
+    let epochs = 20000;
     trainer.train(&mut agent, &env, epochs, true);
 
     // show trained agents
     loop {
-        let mut test_env = FlappyEnvironment::new(env_size);
+        let mut test_env = env.clone();
         let mut score = 0.;
+
         while !test_env.is_done() {
             let state = test_env.observe();
-            //let action_values = agent.get_action_values(&state);
             let action = agent.act(&state);
             score += test_env.step(&action);
 
             // reset cursor position
             print!("\x1B[2J\x1B[1;1H");
-            println!("{}\nscore: {}", test_env, score);
-            //println!("action: {} | action values: {}", action.0, action_values.to_string());
+
+            println!("{}\nscore: {} | action: {}", test_env, score, action.0);
             thread::sleep(time::Duration::from_millis(100));
         }
+
+        thread::sleep(time::Duration::from_millis(300));
     }
 }
